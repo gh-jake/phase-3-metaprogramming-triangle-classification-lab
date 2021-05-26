cbc539e2db7bbceb246098107970bd3c087199ff
Jake Houston <jakechouston@gmail.com>
2021-05-26 19:19:35 -0400

    commit tree 223e0bf21dfe4bb94717e6e34c25181c49215675

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
   # write code here
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind
+    if @side_a > 0 && @side_b > 0 && @side_c > 0 && @side_a + @side_b > @side_c && @side_a + @side_c > @side_b && @side_b + @side_c > @side_a
+        if @side_a == @side_b && @side_b == @side_c
+          :equilateral
+        elsif @side_a == @side_b || @side_b == @side_c || @side_a == @side_c
+          :isosceles
+        else
+          :scalene
+        end
+    else  
+      begin
+        raise TriangleError
+        puts error.message
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "triangle error"
+    end
+  end
 end
